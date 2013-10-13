1ac3336fc422fddbd5913fec90ccd9d2b09f4224
Cheung Chifung <chifung.cheung@gmail.com>
2013-10-13 11:55:56 +0900

    test cdn

--- tool/postprocess.py
+++ tool/postprocess.py
@@ -5,11 +5,11 @@ from os import listdir
 RAW_DATA_PATH = "../raw_data"
 DATA_PATH = "../data"
 
-csvs = [i for i in listdir(RAW_DATA_PATH)if i.endswith(".csv")]
+csvs = [i for i in listdir(RAW_DATA_PATH) if i.endswith(".csv")]
 
 for csv in csvs:
     sourceFileName = "%s/%s" % (RAW_DATA_PATH, csv)
-    targetFileName = "%s/UTF8_%s" % (DATA_PATH, csv)
+    targetFileName = "%s/UTF8_%s.txt" % (DATA_PATH, csv)
     source = open(sourceFileName, "rb")
     target = open(targetFileName, "wb")
     target.write(unicode(source.read(), "sjis").encode("utf-8"))

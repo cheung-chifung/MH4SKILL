638ea55fba1d344aa9cfa1a0f5fefd3bec3e2153
Cheung Chifung <chifung.cheung@gmail.com>
2013-10-13 11:35:24 +0900

    init commit

---
+++ tool/postprocess.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from os import listdir
+
+RAW_DATA_PATH = "../raw_data"
+DATA_PATH = "../data"
+
+csvs = [i for i in listdir(RAW_DATA_PATH)if i.endswith(".csv")]
+
+for csv in csvs:
+    sourceFileName = "%s/%s" % (RAW_DATA_PATH, csv)
+    targetFileName = "%s/UTF8_%s" % (DATA_PATH, csv)
+    source = open(sourceFileName, "rb")
+    target = open(targetFileName, "wb")
+    target.write(unicode(source.read(), "sjis").encode("utf-8"))
